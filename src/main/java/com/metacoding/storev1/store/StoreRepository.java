c01202d9a6ac1b461fadba0532632fe4e6f72f9c
hahamik <dlapdl0615@gmail.com>
2025-03-20 12:18:41 +0900

    store save

--- src/main/java/com/metacoding/storev1/store/StoreRepository.java
+++ src/main/java/com/metacoding/storev1/store/StoreRepository.java
@@ -1,5 +1,28 @@
 package com.metacoding.storev1.store;
 
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+// import lombok.AllArgsConstructor;
+// import lombok.RequiredArgsConstructor;
+import jakarta.persistence.Query;
+
+// @RequiredArgsConstructor //final 붙은 필드로만 생성자 만들어
+//@AllArgsConstructor
+@Repository
 public class StoreRepository {
 
+    private EntityManager em;
+
+    public StoreRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(String name, int stock, int price) {
+        Query query = em.createNativeQuery("insert into store_tb(name,stock,price) values(?,?,?)"); // 자카르타 query
+        query.setParameter(1, name);
+        query.setParameter(2, stock);
+        query.setParameter(3, price);
+        query.executeUpdate(); // insert, update, delete
+    }
 }
--- src/main/java/com/metacoding/storev1/store/StoreService.java
+++ src/main/java/com/metacoding/storev1/store/StoreService.java
@@ -1,5 +1,20 @@
 package com.metacoding.storev1.store;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class StoreService {
+
+    private StoreRepository storeRepository;
+
+    public StoreService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    @Transactional // 이때만 org 기억하자!! jakarta아님!!
+    public void 상품등록(String name, int stock, int price) {
+        storeRepository.save(name, stock, price);
+    }
 
 }
