2f9770c0caf3fbf6613ee1f4f2c46a7479705a37
hahamik <dlapdl0615@gmail.com>
2025-03-20 14:33:25 +0900

    store save

--- src/main/java/com/metacoding/storev1/store/StoreRepository.java
+++ src/main/java/com/metacoding/storev1/store/StoreRepository.java
@@ -1,5 +1,7 @@
 package com.metacoding.storev1.store;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
@@ -24,5 +26,12 @@ public class StoreRepository {
         query.setParameter(2, stock);
         query.setParameter(3, price);
         query.executeUpdate(); // insert, update, delete
+    }
+
+    public List<Store> findAll() {
+        // 조건 : Object Mapping 은 @Entity 가 붙어있어야지만 가능하다.<디폴트 생성자를 호출>
+        // setter 가 없어도 들어가는 이유는?? -> 리플렉션 (setAccessible을 통해)
+        Query query = em.createNativeQuery("select * from store_tb order by id desc", Store.class);
+        return query.getResultList();
     }
 }
